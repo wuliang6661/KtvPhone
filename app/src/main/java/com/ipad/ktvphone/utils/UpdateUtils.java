b2efd1afa7eed99adc304afec21eab5d7dafdfe4
wuliang6661@163.com <wy19941007>
2022-01-17 11:56:34 +0800

    建立心跳连接 ，增加后端静默更新

--- app/src/main/java/com/ipad/ktvphone/utils/UpdateUtils.java
+++ app/src/main/java/com/ipad/ktvphone/utils/UpdateUtils.java
@@ -2,8 +2,13 @@ package com.ipad.ktvphone.utils;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
@@ -13,10 +18,12 @@ import android.util.Log;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ToastUtils;
+import com.ipad.ktvphone.BuildConfig;
 import com.ipad.ktvphone.config.FileConfig;
 import com.ipad.ktvphone.entity.VersionBO;
 
@@ -26,6 +33,7 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -149,6 +157,44 @@ public class UpdateUtils {
             LogUtils.e(Thread.currentThread().getName(), "2");
         }
     };
+
+
+    /**
+     * 安装apk
+     *
+     * @param context
+     * @param filePath 本地apk路径
+     */
+    public static void installApk(Context context, String filePath) {
+        try {
+            File file = new File(filePath);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                    | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+            Uri contentUri;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                contentUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileprovider", file);
+            } else {
+                contentUri = Uri.fromFile(file);
+            }
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+            List<ResolveInfo> resolveLists = context.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            if (resolveLists != null) {
+                for (ResolveInfo resolveInfo : resolveLists) {
+                    if (resolveInfo != null && resolveInfo.activityInfo != null) {
+                        String packageName = resolveInfo.activityInfo.packageName;
+                        context.grantUriPermission(packageName, contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION
+                                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                    }
+                }
+            }
+            context.startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     // 适配android9.0之前的安装方法
